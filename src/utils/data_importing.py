ba8857a20e71eeaf8addc4f12c4736ea1e56953a
Christoffer Rubensson Macbook Pro M2 <choffeofsweden@icloud.com>
2025-08-19 14:41:37 +0200

    Update licence preamble to all source codes

--- src/utils/data_importing.py
+++ src/utils/data_importing.py
@@ -1,3 +1,24 @@
+'''
+SEAMLESS_ZOOM — A technique for seamless zooming between process models and process instances.
+Copyright (C) 2025  Christoffer Rubensson
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Website: https://hu-berlin.de/rubensson
+E-Mail: {firstname.lastname}@hu-berlin.de
+'''
+
 # Load and preprocess event data
 # app/backend/data_loader.py
 from pathlib import Path
